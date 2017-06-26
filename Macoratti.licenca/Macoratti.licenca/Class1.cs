8ac31ab9b19b15f69980c7689650dcc70ea2925d
Gabriel de Sousa <gabriel.de_sousa@hotmail.com>
2017-06-26 09:17:09 -0300

    Init

---
+++ Macoratti.licenca/Macoratti.licenca/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macoratti.licenca
+{
+    public class TipoDeLicenca
+    {
+        public string TRIAL { get; set; }
+    }
+}
